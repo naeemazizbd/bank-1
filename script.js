ecb50220103e86077b45858b718d2748cf85f5a6
Naeem Aziz Chowdhury <74413196+naeemazizbd@users.noreply.github.com>
2020-12-03 10:20:37 +0600

    this is bank code 3 upadate

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//login button js code -1 
+
 const loginBtn=document.getElementById("btnId");
 loginBtn.addEventListener("click", function(){
    const loginArea=document.getElementById('loginArea')
@@ -5,3 +7,51 @@ loginBtn.addEventListener("click", function(){
    const page2=document.getElementById('secondPage')
    page2.style.display="block";
 });
+
+// second page code 
+const depositBtn=document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+   const depositAmount = document.getElementById("depositAmount").value;
+    const depositNumber = parseFloat(depositAmount);
+//    const currentDeposit = document.getElementById("currentDeposit").innerText;
+//    const currentDepositNumber=parseFloat(currentDeposit);
+//   const totalDeposit = depositNumber + currentDepositNumber;
+  
+//   document.getElementById("currentDeposit").innerText = totalDeposit;
+
+  updateSpanText("currentDeposit", depositNumber);
+  
+  updateSpanText("currentBalance", depositNumber);
+
+
+  document.getElementById("depositAmount").value = "";
+
+
+})
+
+// withdraw amount 
+const withdrawBtn=document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+   const withdrawAmount = document.getElementById("withdrawAmount").value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+    const withDrawNumber=getInputNumber("withdrawAmount");
+
+
+
+})
+
+function getInputNumber()
+{
+   const withdrawAmount = document.getElementById("id").value;
+   const withdrawNumber = parseFloat(withdrawAmount);
+   return withdrawNumber;
+
+}
+function updateSpanText (id, depositNumber){
+
+   const current=document.getElementById(id).innerText; 
+  const currentNumber=parseFloat(current);
+  const totalAmount= depositNumber+currentNumber;
+  document.getElementById(id).innerText=totalAmount;
+
+}
