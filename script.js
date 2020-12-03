f56dac60720bf28e00d769b116747b2cb74b828f
Naeem Aziz Chowdhury <74413196+naeemazizbd@users.noreply.github.com>
2020-12-03 20:01:21 +0600

    this is final bank code

--- script.js
+++ script.js
@@ -13,40 +13,35 @@ const depositBtn=document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
    const depositAmount = document.getElementById("depositAmount").value;
     const depositNumber = parseFloat(depositAmount);
-//    const currentDeposit = document.getElementById("currentDeposit").innerText;
-//    const currentDepositNumber=parseFloat(currentDeposit);
-//   const totalDeposit = depositNumber + currentDepositNumber;
-  
-//   document.getElementById("currentDeposit").innerText = totalDeposit;
+
+
 
   updateSpanText("currentDeposit", depositNumber);
   
   updateSpanText("currentBalance", depositNumber);
 
-
   document.getElementById("depositAmount").value = "";
 
 
-})
+});
 
 // withdraw amount 
-const withdrawBtn=document.getElementById("addWithdraw");
+const withdrawBtn= document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click", function(){
-   const withdrawAmount = document.getElementById("withdrawAmount").value;
-    const withdrawNumber = parseFloat(withdrawAmount);
-    const withDrawNumber=getInputNumber("withdrawAmount");
-
+   const withdrawNumber=getInputNumber("withdrawAmount");
+   updateSpanText("currentWithdraw", withdrawNumber);
+   updateSpanText("currentBalance", -1* withdrawNumber);
+   document.getElementById("withdrawAmount").value= "";
 
 
 })
-
-function getInputNumber()
-{
-   const withdrawAmount = document.getElementById("id").value;
-   const withdrawNumber = parseFloat(withdrawAmount);
-   return withdrawNumber;
-
+function getInputNumber(id){
+   const amount=document.getElementById(id).value;
+   const amountNumber=parseFloat(amount);
+   return amountNumber; 
 }
+
+
 function updateSpanText (id, depositNumber){
 
    const current=document.getElementById(id).innerText; 
